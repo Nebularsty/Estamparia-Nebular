c22b38763c97fefd2a354f5a2fa2ade546ab6d31
Nicolas.Oliveira <nicolas.santos@cacaushow.com.br>
2022-11-30 14:38:00 -0300

    adição de data do cliente

--- model/ClienteModel.js
+++ model/ClienteModel.js
@@ -3,19 +3,22 @@ const sequelize = require('sequelize');
 const connection = require('../database/database.js');
 
 const Cliente = connection.define(
-    'tbl_cliente',
-    {
-      nome_cliente:{
-        type: sequelize.STRING(50),
-        allowNull: false
-      },
-      email_cliente:{
-        type: sequelize.STRING(50),
-      },
-      telefone_cliente:{
-        type: sequelize.STRING(15),
-      }
+  'tbl_cliente',
+  {
+    nome_cliente: {
+      type: sequelize.STRING(50),
+      allowNull: false
+    },
+    email_cliente: {
+      type: sequelize.STRING(50),
+    },
+    telefone_cliente: {
+      type: sequelize.STRING(15),
+    },
+    data_nascimento_cliente: {
+      type: sequelize.DATE(),
     }
+  }
 );
 
 module.exports = Cliente;
